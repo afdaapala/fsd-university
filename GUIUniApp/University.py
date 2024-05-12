ea71943e19df32e4057a45baf008613697416e3d
jiyoung yoon <yoon@jiyoungs-MacBook-Pro.local>
2024-05-12 15:46:39 +1000

    modify: gui

--- GUIUniApp/University.py
+++ GUIUniApp/University.py
@@ -1,33 +1,10 @@
-import tkinter as tk
-from tkinter import *
-from Models.Student import StudentGUI
+from Models.Student import Student
 
-class UniversityGUI:
+class University:
     def main(self):
-      root = tk.Tk()
-      student = StudentGUI
-
-      root.geometry("500x200")
-      root.title("Student System")
-      root.configure(bg="#323232")
-      root.resizable(False, False)
-
-      label = tk.Label(root, text="Welcome to Student System",
-                      fg="white", font="Helvetica 18 bold",
-                      padx=20, pady=20)
-      label.pack()
-
-      btn = tk.Button(root, text="A(dmin)", font='Helvetica 14 bold')
-      btn.pack()
-
-      btn = tk.Button(root, text="S(tudent)", font='Helvetica 14 bold', command=student.inputLogin)
-      btn.pack()
-
-      btn = tk.Button(root, text="X(exit)", font='Helvetica 14 bold', command=lambda: root.quit())
-      btn.pack()
-
-      root.mainloop()
+      student = Student
+      student.inputLogin()
 
 if __name__ == "__main__":
-    univ = UniversityGUI()
+    univ = University()
     univ.main()
